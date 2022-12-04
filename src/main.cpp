07169cd3bc6839b2fea9f880149b92692f0632d3
gabor.nemeth <gabor.nemeth@aimotive.com>
2022-12-04 11:05:42 +0100

    Comments added to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "Program.h"
 
 ///
+/// This is the second homework assignment for C++11 Programming lectures at BME.
 /// Source code and installation guide available at:
-///
+/// https://github.com/Gabendi/cpp_bignum
 ///
 
 int main(int argc, const char* argv[]) {
